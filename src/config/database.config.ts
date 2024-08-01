2d9bf82448bbfa2ddf211671b64cb2a2544a7fb1
pawan <pawansubedi221@gmail.com>
2024-08-01 20:17:36 +0545

    first commit

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -4,14 +4,26 @@ import { DataSource } from 'typeorm';
 export const AppDataSource = new DataSource({
   //typeorm config
   type: 'postgres',
-  host: process.env.DATABASE_HOST ,
-  port: parseInt(process.env.DATABASE_PORT) ,
-  username: process.env.DATABASE_USER ,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
+  host: process.env.POSTGRES_HOST,
+  port: parseInt(process.env.POSTGRES_PORT, 10),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
   entities: ['dist/**/*.entity.js'],
   logging: true,
   synchronize: true,
+  ssl: {
+    rejectUnauthorized: false,  // Use this if you need to bypass SSL certificate issues
+  },
+  // type: 'postgres',
+  // host: process.env.DATABASE_HOST ,
+  // port: parseInt(process.env.DATABASE_PORT) ,
+  // username: process.env.DATABASE_USER ,
+  // password: process.env.DATABASE_PASSWORD,
+  // database: process.env.DATABASE_NAME,
+  // entities: ['dist/**/*.entity.js'],
+  // logging: true,
+  // synchronize: true,
   // ssl: true 
 
   // extra:{
--- src/main.ts
+++ src/main.ts
@@ -68,6 +68,7 @@ async function bootstrap() {
     for (let i = 0; i < 6; i++) {
       console.log('.');
     }
+    
   });
 }
 
